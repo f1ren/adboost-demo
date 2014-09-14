3cd6d6a20342be216ceabe309bc824381c2c57c8
adience-code <about@adience.com>
2014-09-14 11:05:40 +0300

    Update DynamicActivity.java

--- AdBoostDemo/src/com/adience/adboost/demo/DynamicActivity.java
+++ AdBoostDemo/src/com/adience/adboost/demo/DynamicActivity.java
@@ -35,22 +35,22 @@ public class DynamicActivity extends Activity {
     private Button showInterstitialButton;
 
     private void initializeAdUnits() {
-	    interstitial = new Interstitial(this);
+	interstitial = new Interstitial(this);
         interstitial.setListener(new InterstitialListener());
-	    Set<SubType> supported = interstitial.getSupportedTypes();
-	    interstitialChoice.clearCheck();
-	    // Show only relevant interstitial subtypes:
-	    for(int i = 0; i < interstitialChoice.getChildCount(); i++) {
-	        View button = interstitialChoice.getChildAt(i);
-	        SubType type = SubType.valueOf((String)button.getTag());
-	        button.setVisibility(supported.contains(type) ? View.VISIBLE : View.GONE);
-	    }
-	    showInterstitialButton.setEnabled(interstitial.isReady()); // Is true if ad network doesn't support caching.
-	    showBannerFromXml();
-	    createBannerProgrammatically();
+	Set<SubType> supported = interstitial.getSupportedTypes();
+	interstitialChoice.clearCheck();
+	// Show only relevant interstitial subtypes:
+	for(int i = 0; i < interstitialChoice.getChildCount(); i++) {
+	    View button = interstitialChoice.getChildAt(i);
+	    SubType type = SubType.valueOf((String)button.getTag());
+	    button.setVisibility(supported.contains(type) ? View.VISIBLE : View.GONE);
 	}
+	showInterstitialButton.setEnabled(interstitial.isReady()); // Is true if ad network doesn't support caching.
+	showBannerFromXml();
+	createBannerProgrammatically();
+    }
 
-	@Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
@@ -133,122 +133,122 @@ public class DynamicActivity extends Activity {
     }
 
     public void loadInterstitial(View view) {
-		SubType subtype = SubType.valueOf((String)view.getTag());
-		if(!interstitial.isReady()) {
-			// Some ad networks don't support caching or caching notification, so isReady will always return true,
-			// in which case don't show the progress indicator.
-		    progress.setVisibility(View.VISIBLE);
-		}
-		interstitial.loadAd(subtype);
+	SubType subtype = SubType.valueOf((String)view.getTag());
+	if(!interstitial.isReady()) {
+	    // Some ad networks don't support caching or caching notification, so isReady will always return true,
+	    // in which case don't show the progress indicator.
+            progress.setVisibility(View.VISIBLE);
+	}
+	interstitial.loadAd(subtype);
     }
     
     public void showInterstitial(View view) {
-	    if(interstitial == null) {
-	        Toast.makeText(this, getText(R.string.first_load_interstitial), Toast.LENGTH_SHORT).show();
-	    } else if(interstitial.isReady()) {
-	        interstitial.show();
-	    } else {
-	        Toast.makeText(this, getText(R.string.interstitial_loading), Toast.LENGTH_SHORT).show();
-	    }
+	if(interstitial == null) {
+	    Toast.makeText(this, getText(R.string.first_load_interstitial), Toast.LENGTH_SHORT).show();
+	} else if(interstitial.isReady()) {
+	    interstitial.show();
+	} else {
+	    Toast.makeText(this, getText(R.string.interstitial_loading), Toast.LENGTH_SHORT).show();
 	}
+    }
 
     
     //////////////// Ad Unit Listeners
 
-	private class BannerListener implements IAdListener {
+    private class BannerListener implements IAdListener {
     	private String prefix;
     	
     	public BannerListener(String prefix) {
-    		this.prefix = prefix;
-		}
+            this.prefix = prefix;
+	}
     	
-		@Override
-		public void adShown() {
-		    Log.i(TAG, prefix + " AdView adShown");
-		}
-
-		@Override
-		public void adReceived() {
-		    Log.i(TAG, prefix + " AdView adReceived");
-		}
-
-		@Override
-		public void adFailed(Object error) {
-		    Log.i(TAG, prefix + " AdView adFailed: " + error);
-		}
-
-		@Override
-		public void adDismissed() {
-		    Log.i(TAG, prefix + " AdView adDismissed");
-		}
-
-		@Override
-		public void adClicked() {
-		    Log.i(TAG, prefix + " AdView adClicked");
-		}
+	@Override
+	public void adShown() {
+	    Log.i(TAG, prefix + " AdView adShown");
 	}
 
-	private class InterstitialListener implements IAdListener {
-		@Override
-		public void adReceived() {
-		    Log.i(TAG, "Interstitial adReceived");
-		    runOnUiThread(new Runnable() {
-		        @Override
-		        public void run() {
-		            showInterstitialButton.setEnabled(true);
-		            progress.setVisibility(View.INVISIBLE);
-		        }
-		    });
-		}
-	
-		@Override
-		public void adFailed(final Object error) {
-		    Log.i(TAG, "Interstitial adFailed");
-		    runOnUiThread(new Runnable() {
-		        @Override
-		        public void run() {
-		            String msg = getString(R.string.interstitial_failed_msg_fmt, error);
-		            Toast.makeText(DynamicActivity.this, msg, Toast.LENGTH_LONG).show();
-		            Log.w(TAG, msg);
-		            interstitialChoice.clearCheck();
-		            progress.setVisibility(View.INVISIBLE);
-		            // Just in case
-		            if(interstitial != null && interstitial.isReady()) {
-		                showInterstitialButton.setEnabled(true);
-		            }
-		        }
-		    });
-		}
-	
-		@Override
-		public void adShown() {
-		    Log.i(TAG, "Interstitial adShown");
-		    // For AdNets that don't support adDismissed:
-		    runOnUiThread(new Runnable() {
-		        @Override
-		        public void run() {
-		            showInterstitialButton.setEnabled(interstitial.isReady());
-		            interstitialChoice.clearCheck();
-		        }
-		    });
-		}
-	
-		@Override
-		public void adClicked() {
-		    Log.i(TAG, "Interstitial adClicked");
-		}
-	
-		@Override
-		public void adDismissed() {
-		    Log.i(TAG, "Interstitial adDismissed");
-		    // For AdNets that don't support adShown:
-		    runOnUiThread(new Runnable() {
-		        @Override
-		        public void run() {
-		            showInterstitialButton.setEnabled(interstitial.isReady());
-		            interstitialChoice.clearCheck();
-		        }
-		    });
-		}
+	@Override
+	public void adReceived() {
+	    Log.i(TAG, prefix + " AdView adReceived");
 	}
+
+	@Override
+	public void adFailed(Object error) {
+	    Log.i(TAG, prefix + " AdView adFailed: " + error);
+	}
+
+	@Override
+	public void adDismissed() {
+	    Log.i(TAG, prefix + " AdView adDismissed");
+	}
+
+	@Override
+	public void adClicked() {
+	    Log.i(TAG, prefix + " AdView adClicked");
+	}
+    }
+
+    private class InterstitialListener implements IAdListener {
+	@Override
+	public void adReceived() {
+	    Log.i(TAG, "Interstitial adReceived");
+	    runOnUiThread(new Runnable() {
+	        @Override
+	        public void run() {
+	            showInterstitialButton.setEnabled(true);
+	            progress.setVisibility(View.INVISIBLE);
+	        }
+	    });
+	}
+	
+	@Override
+	public void adFailed(final Object error) {
+	    Log.i(TAG, "Interstitial adFailed");
+	    runOnUiThread(new Runnable() {
+	        @Override
+	        public void run() {
+	            String msg = getString(R.string.interstitial_failed_msg_fmt, error);
+	            Toast.makeText(DynamicActivity.this, msg, Toast.LENGTH_LONG).show();
+	            Log.w(TAG, msg);
+	            interstitialChoice.clearCheck();
+	            progress.setVisibility(View.INVISIBLE);
+	            // Just in case
+	            if(interstitial != null && interstitial.isReady()) {
+	                showInterstitialButton.setEnabled(true);
+	            }
+	        }
+	    });
+	}
+	
+	@Override
+	public void adShown() {
+	    Log.i(TAG, "Interstitial adShown");
+	    // For AdNets that don't support adDismissed:
+	    runOnUiThread(new Runnable() {
+	        @Override
+	        public void run() {
+	            showInterstitialButton.setEnabled(interstitial.isReady());
+	            interstitialChoice.clearCheck();
+	        }
+	    });
+	}
+	
+	@Override
+	public void adClicked() {
+	    Log.i(TAG, "Interstitial adClicked");
+	}
+
+	@Override
+	public void adDismissed() {
+	    Log.i(TAG, "Interstitial adDismissed");
+	    // For AdNets that don't support adShown:
+	    runOnUiThread(new Runnable() {
+	        @Override
+	        public void run() {
+	            showInterstitialButton.setEnabled(interstitial.isReady());
+	            interstitialChoice.clearCheck();
+	        }
+	    });
+	}
+    }
 }
